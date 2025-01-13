ae37dfb8d80c993a3a4811db52d757c32171753b
Mateus Tomaz <mstomaz11@gmail.com>
2025-01-13 10:14:33 -0300

    Primeiro commit/Adicionando arquivos iniciais

---
+++ blog_rpg/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace blog_rpg.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
